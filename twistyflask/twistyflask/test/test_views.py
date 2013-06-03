4cc175be95f97f4a85a43acd62b624ffaafe26ef
lvh <lvh>
2013-06-03 14:37:27 +0000

    Add a messages table

--- twistyflask/twistyflask/test/test_views.py
+++ twistyflask/twistyflask/test/test_views.py
@@ -34,7 +34,26 @@ class IndexTests(_ViewTestCase):
 
 class ChatTests(_ViewTestCase):
     def test_chat_in_title(self):
+        """
+        The chat view advertises itself in its title.
+        """
         response = self.client.get("/chat")
         self.assertEqual(response.status_code, 200)
         title, = html.fromstring(response.data).cssselect("title")
         self.assertIn("chat channel", title.text.lower())
+
+    def test_has_messages_table(self):
+        """
+        The chat view has a messages table. It has a table header with the
+        appropriate headings, and a table body.
+        """
+        response = self.client.get("/chat")
+        self.assertEqual(response.status_code, 200)
+        table, = html.fromstring(response.data).cssselect("table.messages")
+        time, name, message = table.cssselect("thead td")
+        self.assertIn("time", time.text.lower())
+        self.assertIn("name", name.text.lower())
+        self.assertIn("thoughts", message.text.lower())
+
+        tbody, = table.csselect("tbody")
+        self.assertEqual(tbody.cssselect("tr"), [])
