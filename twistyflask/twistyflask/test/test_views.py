170ee95c63ede922831c2233daa439b78079f3b6
lvh <lvh>
2013-06-04 18:01:10 +0000

    Show log entries when rendering chat page

--- twistyflask/twistyflask/test/test_views.py
+++ twistyflask/twistyflask/test/test_views.py
@@ -1,9 +1,10 @@
+from axiom import store
 from lxml import html
-from twistyflask import app
+from twistyflask import app, log
 from unittest import TestCase
 
 
-class _ViewTestCase(TestCase):
+class _ViewTestCaseMixin(object):
     """
     Common set up and tear down behavior for view tests.
     """
@@ -11,6 +12,7 @@ class _ViewTestCase(TestCase):
         """
         Creates a test client, and puts the app in testing and debug mode.
         """
+        app.store = store.Store()
         self.client = app.test_client()
         app.testing = True
         app.debug = True
@@ -22,38 +24,86 @@ class _ViewTestCase(TestCase):
         app.testing = False
         app.debug = False
 
-class IndexTests(_ViewTestCase):
+    def getTree(self, location):
+        """
+        Gets the page at location, then parses it.
+
+        This explicitly uses a UTF-8 parser to work around a bug in
+        lxml that prevents it from understanding HTML5 <meta> tag
+        encoding specification.
+        """
+        response = self.client.get(location)
+        parser = html.HTMLParser(encoding="utf-8")
+        return html.fromstring(response.data, parser=parser)
+
+    def assertSpecifiesEncoding(self, response):
+        """
+        The page explicitly specifies its encoding, UTF-8, in a meta tag,
+        in the first 1024 bytes of the response. This relies on an exact
+        spelling of that meta tag, as specified in a W3C recommendation.
+        """
+        tag = """<meta charset="UTF-8">"""
+        self.assertIn(tag, response.data[:1024])
+
+class IndexTests(_ViewTestCaseMixin, TestCase):
+    def test_well_behaved(self):
+        """
+        The index returns with a 200 response and specifies its encoding.
+        """
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertSpecifiesEncoding(response)
+
     def test_has_link_to_chat(self):
         """
         The index has a link to the chat page.
         """
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
-        link, = html.fromstring(response.data).cssselect("a[href$=chat]")
+        link, = self.getTree("/").cssselect("a[href$=chat]")
         self.assertIn("click here", link.text.lower())
 
-class ChatTests(_ViewTestCase):
+class ChatTests(_ViewTestCaseMixin, TestCase):
+    def test_well_behaved(self):
+        """
+        The chat page returns with a 200 response and specifies its encoding.
+        """
+        response = self.client.get("/chat")
+        self.assertEqual(response.status_code, 200)
+        self.assertSpecifiesEncoding(response)
+
     def test_chat_in_title(self):
         """
         The chat view advertises itself in its title.
         """
-        response = self.client.get("/chat")
-        self.assertEqual(response.status_code, 200)
-        title, = html.fromstring(response.data).cssselect("title")
+        title, = self.getTree("/chat").cssselect("title")
         self.assertIn("chat channel", title.text.lower())
 
     def test_has_messages_table(self):
         """
         The chat view has a messages table. It has a table header with the
-        appropriate headings, and a table body.
+        appropriate headings, and a table body. The table body contains the
+        messages in the log.
         """
-        response = self.client.get("/chat")
-        self.assertEqual(response.status_code, 200)
-        table, = html.fromstring(response.data).cssselect("table.messages")
+        def makeEntry(sender, message):
+            entry = log.LogEntry(
+                store=app.store, sender=sender, message=message)
+            return entry
+
+        entries = [
+            makeEntry(u"\N{SNOWMAN}", u"\N{RADIOACTIVE SIGN}"),
+            makeEntry(u"\N{FEMALE SIGN}", u"\N{BLACK SUN WITH RAYS}"),
+            makeEntry(u"\N{SNOWMAN}", u"\N{BIOHAZARD SIGN}")
+        ]
+
+        table, = self.getTree("/chat").cssselect("table.messages")
         time, name, message = table.cssselect("thead td")
         self.assertIn("time", time.text.lower())
         self.assertIn("name", name.text.lower())
         self.assertIn("thoughts", message.text.lower())
 
-        tbody, = table.csselect("tbody")
-        self.assertEqual(tbody.cssselect("tr"), [])
+        rows = table.cssselect("tbody tr")
+        self.assertEqual(len(rows), len(entries))
+
+        for row, entry in zip(rows, entries):
+            _time, nameCell, messageCell = row.cssselect("td")
+            self.assertEqual(entry.sender, nameCell.text)
+            self.assertEqual(entry.message, messageCell.text)
--- twistyflask/twistyflask/views.py
+++ twistyflask/twistyflask/views.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from twistyflask import app
+from twistyflask import app, log
 
 @app.route("/")
 def index():
@@ -14,4 +14,7 @@ def chat():
     """
     Renders the chat channel.
     """
-    return render_template("chat.html", title=u"chat channel")
+    E = log.LogEntry
+    entries = app.store.query(E, sort=E.time.ascending)
+    kwargs = {"title": u"chat channel", "entries": entries}
+    return render_template("chat.html", **kwargs)
