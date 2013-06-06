4fb513bf42f267a5d9b6a3e76415f5562a94e1f3
lvh <lvh>
2013-06-06 08:34:01 +0000

    Add store object to app

--- twistyflask/twistyflask/__init__.py
+++ twistyflask/twistyflask/__init__.py
@@ -1,6 +1,16 @@
-from twistyflask._version import __version__
-
+from axiom import store
 from flask import Flask
+from os import environ
+from twistyflask._version import __version__; __version__ # shut up pyflakes
+
 app = Flask("twistyflask")
 
-from twistyflask import views
+def addStore(_environ=environ):
+    """
+    Gets a store path from the environment. If unset, creates in-memory store.
+    """
+    app.store = store.Store(_environ.get("STORE_PATH"))
+
+addStore()
+
+from twistyflask import views; views # shut up pyflakes
